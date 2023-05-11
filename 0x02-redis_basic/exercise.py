6b8f49c4a84fce309b59be48e59b2f431c7fcff6
Certifieddonnie <mcdonaldsamure91@gmail.com>
2023-05-10 17:22:46 -0700

    Tsk 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,7 @@
 Writing strings to Redis
 """
 from __future__ import annotations
+from typing import Union, Callable, Optional
 import redis
 import uuid
 
@@ -18,3 +19,27 @@ class Cache():
         datakey = str(uuid.uuid4())
         self._redis.set(datakey, data)
         return datakey
+
+    def get(self, key: str,
+            fn: Optional[callable] = None) -> Union[str, bytes, int, float]:
+        """ control the return value type """
+        data = self._redis.get(key)
+        if fn:
+            data = fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """ automatically parametrize Cache.get with the correct
+        conversion function."""
+        data = self._redis.get(key)
+        return data.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """ automatically parametrize Cache.get with the
+        correct conversion function. """
+        data = self._redis.get(key)
+        try:
+            data = int(data.decode("utf-8"))
+        except Exception:
+            data = 0
+        return data
